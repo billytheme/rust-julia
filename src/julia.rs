b14b7e18ccfc9816004f8282082ed8d80d6bdc58
Mitchell Reid <mitchellreid02@gmail.com>
2024-04-26 22:54:13 +1000

    Add subsampling for anti-aliasign

--- src/julia.rs
+++ src/julia.rs
@@ -42,25 +42,49 @@ pub fn calc_pixel(
 
     let iteration_bound = 500;
 
-    let equivalent_imaginary = Imaginary {
-        real: (pixel.0 as f64 * pixel_size) - offset.real * (cross_section / 4.0),
-        i: (pixel.1 as f64 * pixel_size) - offset.i * (cross_section / 4.0),
-    };
+    // num_subsamples is in each direction, so the actual number of samples is num_subsamples**2
+    let num_subsamples = 5;
+    let mut subsamples = vec![];
+    let sub_pixel_size = pixel_size / (num_subsamples + 1) as f64;
 
-    match calc_escape(iteration_bound, equivalent_imaginary) {
-        EscapeResult::Bounded => (u8::MIN, u8::MIN, u8::MIN),
-        EscapeResult::Escaped {
-            iter_count,
-            final_val,
-        } => {
-            // https://stackoverflow.com/questions/369438/smooth-spectrum-for-mandelbrot-set-rendering
-            let smoothed_iters = iter_count as f64
-                - f64::log(f64::log(final_val.absolute(), E), E) / f64::log(2.0, E);
-            let scaled_val = smoothed_iters / iteration_bound as f64;
-            hsv_to_rgb((0.95 + 10.0 * scaled_val, 0.6, 1.0))
-            // (scaled_val, scaled_val, 255)
+    for x_pixel_fraction in 1..=num_subsamples {
+        for y_pixel_fraction in 1..=num_subsamples {
+            let equivalent_imaginary = Imaginary {
+                real: (pixel.0 as f64 * pixel_size + x_pixel_fraction as f64 * sub_pixel_size)
+                    - offset.real * (cross_section / 4.0),
+                i: (pixel.1 as f64 * pixel_size + y_pixel_fraction as f64 * sub_pixel_size)
+                    - offset.i * (cross_section / 4.0),
+            };
+
+            let subsample_result = match calc_escape(iteration_bound, equivalent_imaginary) {
+                EscapeResult::Bounded => (u8::MIN, u8::MIN, u8::MIN),
+                EscapeResult::Escaped {
+                    iter_count,
+                    final_val,
+                } => {
+                    // https://stackoverflow.com/questions/369438/smooth-spectrum-for-mandelbrot-set-rendering
+                    let smoothed_iters = iter_count as f64
+                        - f64::log(f64::log(final_val.absolute(), E), E) / f64::log(2.0, E);
+                    let scaled_val = smoothed_iters / iteration_bound as f64;
+                    hsv_to_rgb((0.95 + 10.0 * scaled_val, 0.6, 1.0))
+                    // (scaled_val, scaled_val, 255)
+                }
+            };
+
+            subsamples.push(subsample_result);
         }
     }
+
+    let mut mean_total: (u32, u32, u32) = (0, 0, 0);
+    for sample in subsamples {
+        mean_total.0 += sample.0 as u32;
+        mean_total.1 += sample.1 as u32;
+        mean_total.2 += sample.2 as u32;
+    }
+    mean_total.0 /= num_subsamples * num_subsamples;
+    mean_total.1 /= num_subsamples * num_subsamples;
+    mean_total.2 /= num_subsamples * num_subsamples;
+    return (mean_total.0 as u8, mean_total.1 as u8, mean_total.2 as u8);
 }
 
 // https://www.rapidtables.com/convert/color/hsv-to-rgb.html
